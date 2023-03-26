e287ad0c2fd6cad1200f9a279cada0da137f5225
Quan Pham <quapham27@gmail.com>
2023-03-27 01:50:38 +0700

    update

--- Scripts/jquery.validate.js
+++ Scripts/jquery.validate.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*!
  * jQuery Validation Plugin v1.17.0
  *
@@ -16,6 +17,26 @@
 	}
 }(function( $ ) {
 
+=======
+/*!
+ * jQuery Validation Plugin v1.17.0
+ *
+ * https://jqueryvalidation.org/
+ *
+ * Copyright (c) 2017 Jörn Zaefferer
+ * Released under the MIT license
+ */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+		define( ["jquery"], factory );
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory( require( "jquery" ) );
+	} else {
+		factory( jQuery );
+	}
+}(function( $ ) {
+
+>>>>>>> f5bdbc52153ee7a9cc3b772878dcafb0f7415e75
 $.extend( $.fn, {
 
 	// https://jqueryvalidation.org/validate/
@@ -1561,7 +1582,11 @@ $.extend( $.validator, {
 	}
 
 } );
+<<<<<<< HEAD
 
+=======
+
+>>>>>>> f5bdbc52153ee7a9cc3b772878dcafb0f7415e75
 // Ajax mode: abort
 // usage: $.ajax({ mode: "abort"[, port: "uniqueport"]});
 // if mode:"abort" is used, the previous request on that port (port can be undefined) is aborted via XMLHttpRequest.abort()
@@ -1597,5 +1622,9 @@ if ( $.ajaxPrefilter ) {
 		return ajax.apply( this, arguments );
 	};
 }
+<<<<<<< HEAD
 return $;
+=======
+return $;
+>>>>>>> f5bdbc52153ee7a9cc3b772878dcafb0f7415e75
 }));
--- Views/include/header.php
+++ Views/include/header.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 
@@ -64,4 +65,72 @@
                     </a>
                 </li>
             </ul>
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HQM glasses</title>
+    <link href="../../Content/main.css" rel="stylesheet" />
+    <link href="../../Content/style.css" rel="stylesheet" />
+    <link href="../../Content/Nu.css" rel="stylesheet" />
+    <link href="../../Content/ChiTiet.css" rel="stylesheet" />
+    <link href="../../Content/Zalo.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="line">
+                <h3>
+                    Welcome to HQM glasses!
+                    <h3 style="font-family:none; text-align:right; width:82%"></h3>
+                </h3>
+            </div>
+
+            <div class="menu">
+                <div class="drop">
+                    <a href="../Home/trangchu.php" class="item ">Sản phẩm</a>
+                    <div class="drop-menu">
+                        <a href="../Home/matkinhnu.php">Mắt kính nữ</a>
+                        <a href="../Home/matkinhnam.php">Mắt kính nam</a>
+                        <a href="../Home/matkinhtreem.php">Mắt kính trẻ em</a>
+                    </div>
+                </div>
+
+
+                <a href="#footer-page" class="item">Liên hệ</a>
+
+                <a href="../Home/trangchu.php" class="logo"><img src="../../img/HQM.png" width="180px" height="80px" alt=""></a>
+                <a href="" class="item">Giỏ Hàng</a>
+                <a href="../User/dangky.php" class="item">Đăng ký</a>
+                <a href="../User/dangnhap.php" class="item">Đăng nhập</a>
+
+            </div>
+        </div>
+
+        <nav id="sideNav" style="right: -6px;">
+            <ul>
+                <li>
+                    <a href="../Home/trangchu.php">
+                        <img src="../../img/side_nav_home.png" width="45" height="42" alt="HOME" class="rollover" style="opacity: 1; ">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.fb.com/profile.php?id=100004835897164" onclick="window.open(this.href, 'window', 'width=750, height=550,personalbar=0,toolbar=0,scrollbars=1,resizable=1'); return false;" class="sicon-facebook">
+                        <img src="../../img/side_nav_facebook.png" width="45" height="42" alt="Facebook" class="rollover" style="opacity: 1;">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="../../img/side_nav_facebook.png" width="45" height="42" alt="OFFICIAL SITE" class="rollover" style="opacity: 1;">
+                    </a>
+                </li>
+            </ul>
+>>>>>>> f5bdbc52153ee7a9cc3b772878dcafb0f7415e75
         </nav>
