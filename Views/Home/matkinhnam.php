15b98df6563d73cf37aa4fcf5530f3ea391dd75e
Bao Han <baohanphan.it@gmail.com>
2023-03-29 15:10:23 +0700

    Bảo Hân update giao diện 3 trang (nam, nữ, trẻ em)

---
+++ Views/Home/matkinhnam.php
@@ -0,0 +1,20 @@
+
+<?php include_once("../include/header.php");?>
+
+<div class="navbar">
+            <div class="banner">
+                <img src="../../img/HQM.gif" alt="">
+            </div>
+        </div>
+        <div class="signature">
+            <div class="content">
+                <div class="col-md-9">
+                    <div>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <h2 style="padding-left:775px">Mắt Kính Nam HQM</h2>
+        
+<?php include_once("../include/footer.php");?>
